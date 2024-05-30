c4dbfbdce8192798c4d0b0207dd3b22a3019a362
stanev raven <sstanev@vtechnology.eu>
2024-05-30 13:54:10 +0300

    Routing Requests

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const http = require('http');
 
 // Create a web server
 const server = http.createServer((req, res) => {
+
     console.log(req.url, req.method, req.headers);
     //process.exit() //hard exit event loop
+
+    const url =req.url;
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</buttom></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
