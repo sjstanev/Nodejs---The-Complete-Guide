4b62911a8d969985b6b06d6789dd6ba298294464
stanev raven <sstanev@vtechnology.eu>
2024-05-30 21:48:44 +0300

    How Middleware works

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const express = require('express');
 //create express application
 const app = express();
 
-// `app` is valid request server 
-const server = http.createServer(app);
+// add new middleware function
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); // Allows tge requst to continue to the next middleware in line
+})
+
+// add another middleware function 
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1> Hello from express</h1>')
+})
 
 // Start listener on specific port
-server.listen(4000)
+app.listen(4000)
 
